00df4aafe811afcecc99c618f0586c0037952617
Dakota <95160810+urdakota@users.noreply.github.com>
2023-11-03 13:10:19 -0400

    testing

--- testing/index.js
+++ testing/index.js
@@ -1,5 +1,10 @@
 // Just some functions
 const select = (_, el = document) => el.querySelector(_);
+const create = (_, el = document.body) => {
+  var created = document.createElement(_);
+  el.appendChild(created)
+  return created;
+}
 
 // constants
 const table = ["song", "features", "producer", "description", "date", "length", "type", "quality", "released", "link"];
@@ -152,108 +157,122 @@ async function main() {
   console.log("Loaded Tracker & Formatted");
 
   // Update Website
-  var Title = select(".Header_title__CHLQo");
-  var container = select(".unreleased_container__IqK0q")
+  //var Title = select(".Header_title__CHLQo");
+  //var container = select(".unreleased_container__IqK0q")
   var length = Object.keys(lexed).length;
 
+  console.log(`[${lexed[1].value}] ${length}`);
   // Tracker Title
   //select("span", Title).innerText = lexed[1].value; // Title
   //select("img", Title).src = lexed[1].image; // Image
 
   // Create Albums
-  console.log(lexed[1])
+  var Albums = {}
   for (let i = 1; i < length + 1; i++) {
     var previousDictionary = lexed[length];
     var tokens = lexed[i];
-    console.log(i,tokens);
-	/*
-    if (tokens.token == "Album") {
-      const detailsElement = document.createElement("details");
-      detailsElement.classList.add("EraCard_era__5_ZGH");
-      detailsElement.id = tokens.value
-      container.appendChild(detailsElement);
+    var AlbumsLength = Object.keys(Albums).length;
+		var previousAlbum = Albums[AlbumsLength];
+    var previousAlbumGroupLength;
+    if (!!previousAlbum) previousAlbumGroupLength = Object.keys(previousAlbum.groups).length;
+    
+    if (tokens.token == "Album"){
+    
+      const Holder = create("div");
+      Holder.classList.add("Album");
+      Holder.id = tokens.value
 
-      const summaryElement = document.createElement("summary");
-      summaryElement.style.backgroundColor = tokens.background;
-      detailsElement.appendChild(summaryElement);
+      Albums[AlbumsLength + 1] = {
+        object: Holder,
+        groups: {},
+        songs: {}
+      };
 
-      const imageElement = document.createElement("img");
-      imageElement.alt = tokens.value + " cover";
-      imageElement.src = tokens.image;
-      imageElement.width = 136;
-      imageElement.height = 136;
-      imageElement.decoding = "async";
-      imageElement.dataset.nimg = "1";
-      imageElement.className = "EraCard_cover__6ANFI";
-      imageElement.loading = "lazy";
-      imageElement.style.color = "transparent";
-      summaryElement.appendChild(imageElement);
+      // Decorate
 
-      const TextDiv = document.createElement("div");
-      summaryElement.appendChild(TextDiv);
+      const TextHolder = create("div", Holder);
+      TextHolder.classList.add("Album_Text");
 
-      const titleDiv = document.createElement("div");
-      titleDiv.className = "EraCard_title__Xknl9";
-      TextDiv.appendChild(titleDiv);
+      const AlbumImage = create("img", TextHolder);
+      AlbumImage.classList.add("Album_Image");
+      AlbumImage.src = tokens.image;
 
-      const titleSpan = document.createElement("span");
-      titleSpan.style.color = tokens.textcolor;
-      titleSpan.textContent = tokens.value;
-      titleDiv.appendChild(titleSpan);
+      const AlbumTitle = create("div", TextHolder);
+      AlbumTitle.classList.add("Album_Title");
 
-      const descDiv = document.createElement("div");
-      descDiv.className = "EraCard_description__3MKFd";
-      TextDiv.appendChild(descDiv);
+      const AlbumTitleText = create("Span", AlbumTitle);
+      AlbumTitleText.textContent = tokens.value;
 
-      const descSpan = document.createElement("span");
-      descSpan.style.color = tokens.textcolor;
-      descSpan.textContent = tokens.description;
-      descDiv.appendChild(descSpan);
+      const AlbumDescription = create("div", TextHolder);
+      AlbumDescription.classList.add("Album_Description");
 
-      const tracksDiv = document.createElement("div");
-      tracksDiv.className = "EraCard_tracks__rI0Kj";
-      detailsElement.appendChild(tracksDiv);
+      const AlbumDescriptionText = create("Span", AlbumDescription);
+      AlbumDescriptionText.textContent = tokens.description;
 
-
-      const innerDiv = document.createElement("div");
-      innerDiv.id = "b46fee5b199307af84ee2bc370d94488";
-      innerDiv.setAttribute("tag", "div");
-      innerDiv.className = "Tooltip_tooltip__q1OLA Track_track__j1JOX";
-      innerDiv.setAttribute("content", "No description available.");
-      innerDiv.setAttribute("data-content", "No description available.");
-
-      // Create the first nested div
-      const firstNestedDiv = document.createElement("div");
-
-      // Create a text node for "Hohner Melodica #1" and append it to the first nested div
-      const trackNameText = document.createTextNode("Hohner Melodica #1");
-      firstNestedDiv.appendChild(trackNameText);
-
-      // Create a span element with class "Track_tag__WTlmD" for the "Lossless" tag
-      const tagSpan = document.createElement("span");
-      tagSpan.className = "Track_tag__WTlmD";
-      tagSpan.style.color = "rgb(255, 255, 255)";
-      tagSpan.style.backgroundColor = "rgb(69, 188, 255)";
-      tagSpan.textContent = "Lossless";
-
-      // Append the tag span to the first nested div
-      firstNestedDiv.appendChild(tagSpan);
-
-      // Append the first nested div to the inner div
-      innerDiv.appendChild(firstNestedDiv);
-
-      // Create the second nested div for the track length
-      const secondNestedDiv = document.createElement("div");
-      secondNestedDiv.className = "Track_length__yIb3d";
-      secondNestedDiv.textContent = "0:02";
-
-      // Append the second nested div to the inner div
-      innerDiv.appendChild(secondNestedDiv);
-
-      // Append the inner div to the outer tracks div
-      tracksDiv.appendChild(innerDiv);
+			const Songs = create("div", Holder);
+      Songs.classList.add("Songs");
+      
+      console.log("Created " + tokens.value);
     }
-	*/  
+		if (tokens.token == "Group") {
+    	
+      const Holder = create("div", Albums[AlbumsLength].object);
+      Holder.classList.add("Group");
+      Holder.id = tokens.value
+
+      previousAlbum.groups[previousAlbumGroupLength+1] = {
+        object: Holder,
+        songs: {}
+      };
+
+      // Decorate
+
+      const TextHolder = create("div", Holder);
+      TextHolder.classList.add("Group_Text");
+
+      const AlbumTitle = create("div", TextHolder);
+      AlbumTitle.classList.add("Group_Title");
+
+      const AlbumTitleText = create("Span", AlbumTitle);
+      AlbumTitleText.textContent = tokens.value;
+    	
+			const Songs = create("div", Holder);
+      Songs.classList.add("Songs");
+      
+      console.log("Created " + tokens.value);
+    }
+  	if (tokens.token == "Table") {
+    	
+      var Parent = previousAlbum.songs;
+      if(previousAlbumGroupLength > 0) Parent = previousAlbum.groups[previousAlbumGroupLength].songs;
+      
+      var songs = tokens.value;
+    	var songsLength = Object.keys(songs).length;
+  		for (let i = 1; i < songsLength + 1; i++) {
+      		var song = songs[i];
+        
+          const TextHolder = create("div", Parent);
+          TextHolder.classList.add("Song");
+
+          const SongName = create("div", TextHolder);
+          SongName.classList.add("Song_Name");
+
+          const SongNameText = create("Span", SongName);
+          SongNameText.textContent = song.song;
+      
+          const SongDescription = create("div", TextHolder);
+          SongDescription.classList.add("Song_Description");
+
+          const SongDescriptionText = create("Span", SongDescription);
+          SongDescriptionText.textContent = song.description;
+
+          console.log("Created " + song.song);
+      }
+      // Decorate
+			/*
+    	*/
+    }
+  
   }
 
   // Add the class attribute
@@ -275,7 +294,6 @@ async function main() {
       </div>
   </details>
   */
-
 }
 
 main();
