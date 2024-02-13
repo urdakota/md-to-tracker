bd51e55598c8beeb2da8b81a685dac6af67f2166
Dakota <95160810+urdakota@users.noreply.github.com>
2024-02-12 21:49:52 -0500

    Update index2.js

--- testing/index2.js
+++ testing/index2.js
@@ -4,23 +4,18 @@ import { select, create } from "../dependancies/utils.js";
 
 const filepath = "Ken.json";
 const albumicons = {
-  "More Chaos":
-    "https://i1.sndcdn.com/artworks-jI4WOMsX3U0sZQ1r-MFz42w-t500x500.jpg",
-  "A Great Chaos":
-    "https://upload.wikimedia.org/wikipedia/en/f/f5/Ken_Carson_-_A_Great_Chaos.jpg",
-  X: "https://upload.wikimedia.org/wikipedia/en/f/f8/X_Ken_Carson.jpeg",
-  "Project X":
-    "https://upload.wikimedia.org/wikipedia/en/5/5b/Ken_Carson_Project_X_cover.jpeg",
-  "Lost Files 2": 
-    "https://t2.genius.com/unsafe/863x0/https%3A%2F%2Fimages.genius.com%2Ff67f38cfc7583d6e46363d7e22b23ff9.500x500x1.jpg",
-  "Lost Files":
-    "https://t2.genius.com/unsafe/863x0/https%3A%2F%2Fimages.genius.com%2Fda09dc883513130cddd7d8a86f355f43.500x500x1.jpg",
-  "Teen X: Relapsed": 
-    "https://t2.genius.com/unsafe/863x0/https%3A%2F%2Fimages.genius.com%2F331746ea75acdeb77e04167b01e07bd8.1000x1000x1.png",
-  "Teen X":
-    "https://lastfm.freetls.fastly.net/i/u/300x300/236f34a8d5d1387de307af874add4bd6.jpg",
-  "Boy Barbie":
-    "https://lastfm.freetls.fastly.net/i/u/300x300/1e222749fe03a52f76a7c1e8b643115f.jpg",
+  "More Chaos":       "https://images.genius.com/ddebc30f117a1d77fc3eeb39323c5993.1000x1000x1.jpg",
+  "A Great Chaos":    "https://images.genius.com/7691b69f0717dacd9c678521d722160a.1000x1000x1.png",
+  "Lost ?":           "https://images.genius.com/b2c460bf426b9df10f6073d8f15175a4.1000x1000x1.png",
+  "X":                "https://images.genius.com/de650f4d0ca39dbf04a5d6b51d8a84a8.1000x1000x1.png",
+  "X-23":             "https://images.genius.com/0ad831c7dff193912d48b18ecbfc6b3b.252x252x1.jpg",
+  "Lost Files 3":     "https://images.genius.com/3750b1d6541912c482aa06a5a5fc426b.500x500x1.jpg",
+  "Project X":        "https://images.genius.com/d3a0f08457e58a24e5c4cf357f23b987.1000x1000x1.png",
+  "Lost Files 2":     "https://images.genius.com/f67f38cfc7583d6e46363d7e22b23ff9.500x500x1.jpg",
+  "Lost Files":       "https://images.genius.com/da09dc883513130cddd7d8a86f355f43.500x500x1.jpg",
+  "Teen X: Relapsed": "https://images.genius.com/331746ea75acdeb77e04167b01e07bd8.1000x1000x1.png",
+  "Teen X":           "https://images.genius.com/b29cb9082c5b354d09b3c23f3a7402ec.1000x1000x1.png",
+  "Boy Barbie":       "https://images.genius.com/1ef69452d04b5a151fe7738c0b633171.1000x1000x1.jpg",
 };
 const currentDate = new Date();
 
@@ -118,9 +113,44 @@ function requestdata() {
     });
 }
 function loadcontent(jsonData) {
+  const albums_wrapper = select("#albums > .wrapper");
   var recentsongs = {};
   for (const key in jsonData) {
     var album = key;
+    
+    var albumelement = create("div");
+    albumelement.style.display = "inline-block";
+    albumelement.classList.add("album-element");
+    albums_wrapper.insertBefore(albumelement, albums_wrapper.firstChild);
+
+    var albumart = create("img", albumelement);
+    albumart.src = albumicons[album];
+    albumart.classList.add("noselect");
+    albumart.height = "200";
+    albumart.width = "200";
+    albumart.style.margin = "6px";
+    albumart.style["margin-bottom"] = "0px";
+    albumart.style["border-radius"] = "6px";
+
+    var songname = create("div", albumelement);
+    songname.classList.add("song-name");
+    songname.style.margin = "6px";
+    songname.style["margin-top"] = "0px";
+    songname.style["margin-bottom"] = "0px";
+    songname.style.padding = "0px";
+    songname.textContent = album;
+
+    var songartist = create("div", albumelement);
+    songartist.classList.add("song-artist");
+    songartist.style.margin = "6px";
+    songartist.style["margin-top"] = "0px";
+    songartist.style["margin-bottom"] = "0px";
+    songartist.style.padding = "0px";
+    var betterdate = "";
+    if (jsonData[album]["leak date"] != "Unknown")
+      betterdate = `${formatDate(jsonData[album]["leak date"])}`;
+    songartist.textContent = `${betterdate}`;
+
     for (const key in jsonData[album]) {
       if (
         jsonData[album].hasOwnProperty(key) &&
@@ -135,7 +165,6 @@ function loadcontent(jsonData) {
     }
   }
   recentsongs = sortByDate(recentsongs);
-  console.log(recentsongs)
   const songrow1 = select("#row1");
   const songrow2 = select("#row2");
   var new8songs = Object.entries(recentsongs).slice(0, 8);
